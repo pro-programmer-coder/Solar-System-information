19084e3d655d62bda1eab2895ccecec2339fbf45
pro-programmer-coder <lawrencej.london2019a@gmail.com>
2020-01-14 11:11:11 +0000

    correct userID input

--- src/main/java/Core/SolarInformation/SolarSystemInformation.java
+++ src/main/java/Core/SolarInformation/SolarSystemInformation.java
@@ -1,5 +1,13 @@
 package Core.SolarInformation;
 
 public class SolarSystemInformation {
+    private String userID;
 
+    public SolarSystemInformation(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
 }
