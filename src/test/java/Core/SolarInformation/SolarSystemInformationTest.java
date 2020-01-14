86721ac3804898ac704c244b740eef6739c6c8e3
pro-programmer-coder <lawrencej.london2019a@gmail.com>
2020-01-14 10:59:05 +0000

    Editted Valid userID test created

--- src/test/java/Core/SolarInformation/SolarSystemInformationTest.java
+++ src/test/java/Core/SolarInformation/SolarSystemInformationTest.java
@@ -2,19 +2,17 @@ package Core.SolarInformation;
 
 import org.junit.jupiter.api.Test;
 
-import static org.easymock.EasyMock.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolarSystemInformationTest {
 
     @Test
     public void validUserID(){
-        String actualUserId = "AB1234";
+        String actualUserID = "AB1234";
+        String expected = "AB1234";
+        SolarSystemInformation solarSystemInformation = new SolarSystemInformation(actualUserID);
 
-        SolarSystemInformation solarSystemInformation = new SolarSystemInformation(actualUserId);
-
-        expect(solarSystemInformation.authenticate().once());
-        replay(solarSystemInformation);
-
+        assertEquals(expected, solarSystemInformation.getUserID());
     }
 }
