8455064d69fcccb08fea9b1e82f8fc4415443f5a
pro-programmer-coder <lawrencej.london2019a@gmail.com>
2020-01-15 13:57:57 +0000

    setter for valid object name setter

--- src/test/java/Core/SolarInformation/SolarSystemInformationTest.java
+++ src/test/java/Core/SolarInformation/SolarSystemInformationTest.java
@@ -461,4 +461,21 @@ class SolarSystemInformationTest {
 
         verify(mockWebService);
     }
+
+    @Test
+    public void testSetterForAstronomicalObjectClassificationObjectNameWithValidCode() throws Exception {
+        String actualUserID = "AB1234";
+        String actualUserPassword = "AaBbCc1!2?";
+        String actualAstronomicalObjectClassificationCode = "PMer58M";
+        IWebService mockWebService = EasyMock.createMock(IWebService.class);
+        SolarSystemInformation solarSystemInformation = new SolarSystemInformation(actualUserID, actualUserPassword, mockWebService);
+
+        expect(mockWebService.getStatusInfo(actualAstronomicalObjectClassificationCode)).andReturn("PMer58M,Planet,Mercury,88,2439.4,57909050,330110000000000000000000");
+        replay(mockWebService);
+
+        solarSystemInformation.initialiseAOCDetailsValidate("PMer58M");
+        assertEquals("Mercury", solarSystemInformation.getObjectName());
+
+        verify(mockWebService);
+    }
 }
